bf4d169a0b240719467b10cb90c4d9f79148f2d1
Rahat Hossain <rahat1470.com@gmail.com>
2025-10-20 00:25:19 +0600

    update

---
+++ lib/Fragment/PhoneFragment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class PhoneFragment extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Phone Fragment'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,71 @@
+import 'package:tab_bar/Fragment/ContactFragments.dart';
+import 'package:tab_bar/Fragment/EmailFragment.dart';
+import 'package:tab_bar/Fragment/HomeFragment.dart';
+import 'package:tab_bar/Fragment/PersonFragment.dart';
+import 'package:tab_bar/Fragment/PhoneFragment.dart';
+import 'package:tab_bar/Fragment/SearchFragment.dart';
+import 'package:tab_bar/Fragment/SettingsFragment.dart';
+
+import './Fragment/AccountBalanceFragment.dart';
+import 'package:flutter/material.dart';
+import 'package:tab_bar/Fragment/AccountBalanceFragment.dart';
+
 void main(){
-  runAp
+  runApp(const MyApplication());
+}
+
+class MyApplication extends StatelessWidget{
+  const MyApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeActivity(),
+      title: 'Rahat Hossain',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 8,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Rahat Hossain',style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.blue,
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home',),
+              Tab(icon: Icon(Icons.search), text: 'Search',),
+              Tab(icon: Icon(Icons.settings), text: 'Settings',),
+              Tab(icon: Icon(Icons.email), text: 'Email',),
+              Tab(icon: Icon(Icons.contact_mail), text: 'Contact Mail',),
+              Tab(icon: Icon(Icons.person), text: 'Person',),
+              Tab(icon: Icon(Icons.phone), text: 'Phone',),
+              Tab(icon: Icon(Icons.account_balance), text: 'Account Balance',),
+            ],
+            isScrollable: true,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            HomeFragment(),
+            SearchFragment(),
+            SettingsFragment(),
+            EmailFragment(),
+            ContactFragment(),
+            PersonFragment(),
+            PhoneFragment(),
+            AccountBalanceFragment(),
+          ],
+        ),
+      ),
+    );
+  }
 }
